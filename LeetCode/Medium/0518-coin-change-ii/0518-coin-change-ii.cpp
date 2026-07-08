2eaf6e658abb3f45a4a671244d2251a64fe072f7
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 14:47:23 +0530

    Time: 5 ms (90.24%), Space: 10.1 MB (94%) - LeetHub

---
+++ LeetCode/Medium/0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:
+    int solve(int n, vector<int>& coins, int amount) {
+
+        vector<unsigned int> dp (amount + 1, 0);
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j++) {
+                dp[j] += dp[j - coin];
+            }
+        }
+        return (int)dp[amount];
+    }
+
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        return solve(n, coins, amount);
+    }
+};
