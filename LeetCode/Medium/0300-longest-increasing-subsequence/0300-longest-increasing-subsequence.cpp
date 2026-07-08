870afb502b1d91965357d01ee2fee7e64780f599
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 14:54:35 +0530

    Time: 3 ms (79.21%), Space: 14.1 MB (87.11%) - LeetHub

---
+++ LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int solve(int n, vector<int>& nums,vector<int>& ans) {
+if (n==0) {
+    return 0;
+}
+
+ans.push_back(nums[0]);
+for (int i=1;i<n;i++) {
+    if (nums[i]>ans.back()) {
+        ans.push_back(nums[i]);
+    }
+
+    else {
+        int index=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+        ans[index]=nums[i];
+    }
+}
+
+return ans.size();
+    }
+
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        return solve(n, nums,ans);
+    }
+};
