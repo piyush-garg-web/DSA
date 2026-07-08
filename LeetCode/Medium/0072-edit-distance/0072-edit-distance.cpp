a613cc91788ef5d20ab79e647c341502837ec8e0
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 15:03:02 +0530

    Time: 5 ms (72.88%), Space: 10.4 MB (96.77%) - LeetHub

---
+++ LeetCode/Medium/0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,48 @@
+class Solution {
+private:
+    int solve(string& word1, string& word2) {
+
+        vector<int> curr (word2.length()+1,0);
+         vector<int> next (word2.length()+1,0);
+
+        for (int j = 0; j < word2.length(); j++) {
+            next[j] = word2.length() - j;
+        }
+
+        for (int i = word1.length() - 1; i >= 0; i--) {
+            for (int j = word2.length() - 1; j >= 0; j--) {
+
+                curr[word2.length()]=word1.length() - i;
+                int ans = 0;
+
+                if (word1[i] == word2[j]) {
+                    ans=next[j+1];
+                }
+
+                else {
+                    int insert = 1 + curr[j+1];
+                    int del = 1 + next[j];
+                    int replace = 1 + next[j+1];
+                    ans = min(insert, min(del, replace));
+                }
+
+                curr[j] = ans;
+            }
+            next=curr;
+        }
+        return next[0];
+    }
+
+public:
+    int minDistance(string word1, string word2) {
+        if (word1.length() == 0) {
+            return word2.length();
+        }
+
+        if (word2.length() == 0) {
+            return word1.length();
+        }
+
+        return solve(word1, word2);
+    }
+};
