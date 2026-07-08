4de2afc14c2631691de21dd626a699320d9626df
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 14:26:57 +0530

    Time: 0 ms (100%), Space: 37.8 MB (37.38%) - LeetHub

---
+++ LeetCode/Easy/0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    private: 
+    void inorder(TreeNode* root, vector<int>& nodes) {
+        if (root==NULL) {
+            return;
+        }
+        inorder(root->left,nodes);
+        nodes.push_back(root->val);
+        inorder(root->right,nodes);
+    }
+
+    bool isSumPresent (vector<int> nodes,int k) {
+        int i=0;
+        int j=nodes.size()-1;
+        while (i<j) {
+            if (nodes[i]+nodes[j]==k) {
+                return true;
+            }
+            else if (nodes[i]+nodes[j]>k) {
+                j--;
+            }
+
+            else {
+                i++;
+            }
+        }
+        return false;
+    }
+
+public:
+    bool findTarget(TreeNode* root, int k) {
+        vector<int> nodes;
+        inorder(root,nodes);
+        return isSumPresent(nodes,k);
+
+
+        
+    }
+};
