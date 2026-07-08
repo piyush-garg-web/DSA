d00df77e3a5e413f7b4f2ab5df2179000344924c
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 14:44:21 +0530

    Time: 28 ms (84.29%), Space: 14.7 MB (84.94%) - LeetHub

---
+++ LeetCode/Medium/1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+
+        int n = points.size();
+        int minCost = 0;
+        vector<int> minDist(n, INT_MAX);
+        vector<bool> visited(n, 0);
+
+        minDist[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            int mini = INT_MAX;
+            int u=-1;
+            for (int j = 0; j < n; j++) {
+                if (visited[j] != 1 && minDist[j] < mini) {
+                    u = j;
+                    mini=minDist[j];
+                }
+            }
+
+            visited[u] = 1;
+            minCost += minDist[u];
+
+            for (int k = 0; k < n; k++) {
+                int distance = abs(points[u][0] - points[k][0]) +
+                               abs(points[u][1] - points[k][1]);
+                if (!visited[k] && distance < minDist[k]) {
+                    minDist[k] = distance;
+                }
+            }
+        }
+
+        return minCost;
+    }
+};
