8728a080a2c8d80c0db607cbddf8718212299666
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 13:55:43 +0530

    Time: 203 ms (36.44%), Space: 12.7 MB (64.83%) - LeetHub

---
+++ LeetCode/Medium/0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        int count =0;
+        if (n<=2) {
+            return 0;
+        }
+        vector <bool> isPrime (n,1) ;
+        isPrime[0]=isPrime[1]=0;
+        for (int i=2;i<n;i++) {
+            if (isPrime[i]) {
+                count++;
+                for (int j=2*i;j<n;j+=i) {
+                    isPrime[j]=0;
+                }
+            }
+        }
+return count;
+    }
+};
