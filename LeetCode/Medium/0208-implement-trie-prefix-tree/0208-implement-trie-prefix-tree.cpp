91f797cb408bd295860c79a4eb4dd13fb469174c
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 14:34:49 +0530

    Time: 73 ms (5.79%), Space: 142.2 MB (5.02%) - LeetHub

---
+++ LeetCode/Medium/0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,98 @@
+class TrieNode {
+public:
+    char ch;
+    TrieNode* children[26];
+    bool isTerminal;
+
+    TrieNode(char ch) {
+        this->ch = ch;
+        for (int i = 0; i < 26; i++) {
+            children[i] = NULL;
+        }
+        isTerminal = false;
+    }
+};
+
+class Trie {
+    TrieNode* root;
+
+public:
+    Trie() { root = new TrieNode('\0'); }
+
+    void insertUtil(TrieNode* root, string word) {
+        if (word.length() == 0) {
+            root->isTerminal = true;
+            return;
+        }
+
+        TrieNode* child;
+
+        int index = word[0] - 'a';
+        if (root->children[index] != NULL) {
+            child = root->children[index];
+        }
+
+        else {
+            child = new TrieNode(word[0]);
+            root->children[index] = child;
+        }
+
+        insertUtil(child, word.substr(1));
+    }
+
+    void insert(string word) { insertUtil(root, word); }
+
+    bool searchUtil(TrieNode* root, string word) {
+        if (word.length() == 0) {
+            return root->isTerminal;
+        }
+
+        TrieNode* child;
+
+        int index = word[0] - 'a';
+        if (root->children[index] != NULL) {
+            child = root->children[index];
+        }
+
+        else {
+           return false;
+        }
+
+        return searchUtil(child, word.substr(1));
+    }
+
+    bool search(string word) {
+        return searchUtil(root,word);
+    }
+
+    bool startsWithUtil(TrieNode* root, string prefix) {
+        if (prefix.length() == 0) {
+            return true;
+        }
+
+        TrieNode* child;
+
+        int index = prefix[0] - 'a';
+        if (root->children[index] != NULL) {
+            child = root->children[index];
+        }
+
+        else {
+           return false;
+        }
+
+        return startsWithUtil(child, prefix.substr(1));
+    }
+
+    bool startsWith(string prefix) {
+        return startsWithUtil(root,prefix);
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
