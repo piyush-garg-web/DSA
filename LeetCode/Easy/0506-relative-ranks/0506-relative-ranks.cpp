61f47865f37144566b84ba57ebaba228edf6c875
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 14:31:42 +0530

    Time: 4 ms (62.23%), Space: 15.6 MB (65.91%) - LeetHub

---
+++ LeetCode/Easy/0506-relative-ranks/0506-relative-ranks.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        int n = score.size();
+        priority_queue<pair<int, int>> pq;
+        for (int i = 0; i < n; i++) {
+            pq.push({score[i], i});
+        }
+
+        vector<string> ans(n);
+        int place = 1;
+
+        while (!pq.empty()) {
+            int score=pq.top().first;
+            int position=pq.top().second;
+            if (place==1) {
+                ans[position]="Gold Medal";
+            }
+            else if (place==2) {
+                ans[position]="Silver Medal";
+            }
+            else if (place==3) {
+                ans[position]="Bronze Medal";
+            }
+            else {
+                ans[position]=to_string(place);
+            }
+            place++;
+            pq.pop();
+        }
+        return ans;
+    }
+};
