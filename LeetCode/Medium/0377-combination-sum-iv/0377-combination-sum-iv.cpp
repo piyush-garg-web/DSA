25708d737720c6d57f06968d23baa64defea4a1f
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 14:51:27 +0530

    Time: 3 ms (38.23%), Space: 9.3 MB (19.48%) - LeetHub

---
+++ LeetCode/Medium/0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    int solve(int sum, vector<int>& nums, int target) {
+
+        vector<unsigned long long> dp(target + 1, 0);
+        dp[0] = 1;
+
+        for (int i = 1; i <= target; i++) {
+            for (int j = 0; j < nums.size(); j++) {
+                if (i >= nums[j]) {
+                    dp[i]+= dp[i - nums[j]];
+                }
+            }
+        }
+
+        return (int)dp[target];
+    }
+
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        return solve(0, nums, target);
+    }
+};
