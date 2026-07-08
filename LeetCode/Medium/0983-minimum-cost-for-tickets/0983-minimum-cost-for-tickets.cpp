11d2bbda0c599de29b694a8f6679142cfdef6a96
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 14:52:02 +0530

    Time: 4 ms (15.29%), Space: 13.2 MB (77.26%) - LeetHub

---
+++ LeetCode/Medium/0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        queue<pair<int, int>> week;
+        queue<pair<int, int>> month;
+        int ans = 0;
+
+        for (int day : days) {
+            while (!week.empty() && day >= week.front().first+7) {
+                week.pop();
+            }
+
+            while (!month.empty() && day >= month.front().first+30) {
+                month.pop();
+            }
+
+            week.push({day, ans + costs[1]});
+            month.push({day, ans + costs[2]});
+
+            ans = min(ans + costs[0],
+                      min(week.front().second, month.front().second));
+        }
+
+        return ans;
+    }
+};
