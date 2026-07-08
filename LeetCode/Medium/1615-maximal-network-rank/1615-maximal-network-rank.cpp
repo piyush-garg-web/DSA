f8b2d967039a5c096be2784032fd6a1b91fe911f
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 14:40:49 +0530

    Time: 34 ms (37.35%), Space: 43.8 MB (20.62%) - LeetHub

---
+++ LeetCode/Medium/1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        vector<int> degree (n,0);
+        unordered_map<int,unordered_set<int>> adj;
+        for (int i=0;i<roads.size();i++) {
+            int u=roads[i][0];
+            int v=roads[i][1];
+            degree[u]++;
+            degree[v]++;
+            adj[u].insert(v);
+            adj[v].insert(u);
+
+        }
+
+        int ans=0;
+
+for (int i=0;i<n;i++) {
+    for (int j=i+1;j<n;j++) {
+        int rank=degree[i]+degree[j];
+        if (adj[i].count(j)) {
+            rank--;
+        }
+        ans=max(ans,rank);
+
+    }
+}
+
+return ans;
+        
+    }
+};
