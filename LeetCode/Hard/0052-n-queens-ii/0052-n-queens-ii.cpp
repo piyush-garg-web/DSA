21026cdd4869105b0c60eaadf63ec1bf5a9fac5c
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 14:36:28 +0530

    Time: 2 ms (54.38%), Space: 8.5 MB (31.81%) - LeetHub

---
+++ LeetCode/Hard/0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+private:
+    void solve(int col, int n, int& count,
+               vector<string>& board, vector<bool>& rowCheck,
+               vector<bool>& lowerDiagonalCheck,
+               vector<bool>& upperDiagonalCheck) {
+        if (col == n) {
+            count++;
+            return;
+        }
+
+        for (int row = 0; row < n; row++) {
+            if (!rowCheck[row] && !lowerDiagonalCheck[row + col] &&
+                !upperDiagonalCheck[n - 1 + col - row]) {
+                board[row][col] = 'Q';
+                rowCheck[row] = 1;
+                lowerDiagonalCheck[row + col] = 1;
+                upperDiagonalCheck[n - 1 + col - row] = 1;
+                solve(col + 1, n, count, board, rowCheck, lowerDiagonalCheck,
+                      upperDiagonalCheck);
+                board[row][col] = '.';
+                rowCheck[row] = 0;
+                lowerDiagonalCheck[row + col] = 0;
+                upperDiagonalCheck[n - 1 + col - row] = 0;
+            }
+        }
+    }
+
+public:
+    int totalNQueens(int n) {
+        vector<string> board(n, string(n, '.'));
+      int count=0;
+        vector<bool> rowCheck(n, false);
+        vector<bool> lowerDiagonalCheck(2 * n - 1, false);
+        vector<bool> upperDiagonalCheck(2 * n - 1, false);
+        solve(0, n, count, board, rowCheck, lowerDiagonalCheck,
+              upperDiagonalCheck);
+        return count;
+    }
+};
