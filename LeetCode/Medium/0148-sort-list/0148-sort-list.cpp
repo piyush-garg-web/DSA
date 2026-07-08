7cec10fd07a461c0d6e7748935e6de799768af06
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 14:05:36 +0530

    Time: 79 ms (5.75%), Space: 86.2 MB (5.74%) - LeetHub

---
+++ LeetCode/Medium/0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    private:
+    ListNode* middleNode(ListNode* head) {
+        ListNode*slow=head;
+        ListNode*fast=head->next;
+        while (fast!=NULL && fast->next!=NULL) {
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        return slow;
+    }
+    ListNode* merge(ListNode* left,ListNode* right) {
+        ListNode* dummy=new ListNode(-1);
+        ListNode* temp=dummy;
+        while (left!=NULL && right!=NULL) {
+            if (left->val<=right->val) {
+                temp->next=left;
+                temp=left;
+                left=left->next;
+            }
+            else{
+                temp->next=right;
+                temp=right;
+                right=right->next;
+            }
+        }
+        while (left!=NULL) {
+            temp->next=left;
+            temp=left;
+            left=left->next;
+        }
+        while (right!=NULL) {
+            temp->next=right;
+            temp=right;
+            right=right->next;
+        }
+       ListNode*ans=dummy->next;
+       dummy->next=NULL;
+       delete(dummy);
+       return ans;
+    }
+public:
+    ListNode* sortList(ListNode* head) {
+        if (head==NULL || head->next==NULL) {
+            return head;
+        }
+        ListNode*left=head;
+        ListNode*middle=middleNode(head);
+        ListNode* right=middle->next;
+        middle->next=NULL;
+        left=sortList(left);
+        right=sortList(right);
+        return merge(left,right);
+
+    }
+};
