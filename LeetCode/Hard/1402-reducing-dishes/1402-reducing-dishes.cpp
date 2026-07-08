92b8144ac9592e4bae368bf3039d81e660712fda
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 14:53:56 +0530

    Time: 4 ms (54.94%), Space: 12.1 MB (66.54%) - LeetHub

---
+++ LeetCode/Hard/1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    int solve(int n, vector<int>& satisfaction) {
+
+        vector<int> curr(n + 1, 0);
+        vector<int> next(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
+                int include = satisfaction[i] * (j + 1) + next[j + 1];
+                int exclude = 0 + next[j];
+                curr[j] = max(include, exclude);
+            }
+            next = curr;
+        }
+
+        return next[0];
+    }
+
+public:
+    int maxSatisfaction(vector<int>& satisfaction) {
+        int n = satisfaction.size();
+        sort(satisfaction.begin(), satisfaction.end());
+        return solve(n, satisfaction);
+    }
+};
