f6b146d685c4526f414e4ead92880f2e763bf9a4
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 14:06:36 +0530

    Time: 0 ms (100%), Space: 8 MB (63.54%) - LeetHub

---
+++ LeetCode/Medium/0856-score-of-parentheses/0856-score-of-parentheses.cpp
@@ -0,0 +1,31 @@
+#include <stack>
+class Solution {
+
+public:
+    int scoreOfParentheses(string s) {
+        int score = 0;
+        stack<int> st;
+        for (int i = 0; i < s.length(); i++) {
+            int val = 0;
+            char ch = s[i];
+            if (ch == '(') {
+                st.push(0);
+            } else {
+                while (st.top() != 0) {
+                    val = val + st.top();
+                    st.pop();
+                }
+
+                val = max(2 * val, 1);
+                st.pop();
+                st.push(val);
+            }
+        }
+    
+    while (!st.empty()) {
+        score = score + st.top();
+        st.pop();
+    }
+    return score;
+    }
+};
