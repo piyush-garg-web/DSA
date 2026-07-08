bb58c83fccc9b9a48af2c6e977b167bd5f9cae86
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 14:50:45 +0530

    Time: 34 ms (96.3%), Space: 15 MB (76.9%) - LeetHub

---
+++ LeetCode/Medium/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    bool solve(vector<int>& nums, int n, int target) {
+
+        vector<int> dp(target + 1, 0);
+
+        dp[0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = target; j >= nums[i]; j--) {
+                dp[j] = dp[j] || dp[j - nums[i]];
+            }
+        }
+        return dp[target];
+    }
+
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+
+        if (sum % 2 != 0) {
+            return false;
+        }
+
+        else {
+
+            return (solve(nums, n, sum / 2));
+        }
+    }
+};
