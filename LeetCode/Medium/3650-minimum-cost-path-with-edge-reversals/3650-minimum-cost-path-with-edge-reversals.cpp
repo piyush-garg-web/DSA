90ec3ace920ba01b20d3163e5e1664b329608729
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 14:44:01 +0530

    Time: 353 ms (30.65%), Space: 342.9 MB (12.94%) - LeetHub

---
+++ LeetCode/Medium/3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minCost(int n, vector<vector<int>>& edges) {
+        unordered_map<int,list<pair<int,int>>> adj;
+        for (int i=0;i<edges.size();i++) {
+            int u=edges[i][0];
+            int v=edges[i][1];
+            int w=edges[i][2];
+            adj[u].push_back(make_pair(v,w));
+            adj[v].push_back(make_pair(u,2*w));
+        }
+
+        vector<int> distance (n,INT_MAX);
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        pq.push(make_pair(0,0));
+        distance[0]=0;
+        while(!pq.empty()) {
+            auto top=pq.top();
+            pq.pop();
+            
+            if (top.first>distance[top.second]) {
+                continue;
+            }
+            
+            if (top.second==n-1) {
+                return top.first;
+            }
+
+            for (auto nbr:adj[top.second]) {
+                if (top.first+nbr.second<distance[nbr.first]) {
+                    pq.push(make_pair(top.first+nbr.second,nbr.first));
+                    distance[nbr.first]=top.first+nbr.second;
+                }
+            }
+        }
+
+        return -1;
+        
+    }
+};
