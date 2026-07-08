8c7d21fa6bfd952fa0c7db5f81b89a8ceef1dcd0
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 15:04:32 +0530

    Time: 0 ms (100%), Space: 10.2 MB (69.99%) - LeetHub

---
+++ LeetCode/Medium/0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string temp = "";
+        string ans = "";
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            while (i >= 0 && s[i] == ' ') {
+                i--;
+            }
+
+            while (i >= 0 && s[i] != ' ') {
+                temp.push_back(s[i]);
+                i--;
+            }
+
+            reverse(temp.begin(), temp.end());
+            if (temp.length() > 0) {
+                ans += temp;
+                ans += ' ';
+                temp = "";
+            }
+        }
+
+        ans.pop_back();
+        return ans;
+    }
+};
