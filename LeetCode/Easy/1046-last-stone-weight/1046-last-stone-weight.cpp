e8f50266469d63024c681c6c3bcc175439bd3acc
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 14:30:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Easy/1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,28 @@
+#include<queue>
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for (int i=0;i<stones.size();i++) {
+            pq.push(stones[i]);
+        }
+
+        while (pq.size()>1) {
+            int a=pq.top();
+            pq.pop();
+            int b=pq.top();
+            pq.pop();
+            if (b!=a) {
+                pq.push(a-b);
+            }
+        }
+
+        if (pq.empty()) {
+            return 0;
+        }
+        else {
+            return pq.top();
+        }
+        
+    }
+};
