ef71d406120068e8d859f6e20bbd659fed4f7c8b
PIYUSH GARG <piyushgarg6702@gmail.com>
2026-07-08 15:03:31 +0530

    Time: 41 ms (71.41%), Space: 11 MB (80.81%) - LeetHub

---
+++ LeetCode/Hard/0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    bool solve(string& s, string& p) {
+
+        vector<int> curr(p.length() + 1, 0);
+        vector<int> next(p.length() + 1, 0);
+
+        next[p.length()] = 1;
+
+        for (int j = p.length() - 1; j >= 0; j--) {
+            bool flag = true;
+
+            for (int k = j; k < p.length(); k++) {
+                if (p[k] != '*') {
+                    flag = false;
+                    break;
+                }
+            }
+
+            next[j] = flag;
+        }
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = p.length() - 1; j >= 0; j--) {
+
+                if (s[i] == p[j] || p[j] == '?') {
+                    curr[j] = next[j + 1];
+                }
+
+                else if (p[j] == '*') {
+                    curr[j] = (curr[j + 1] || next[j]);
+                }
+
+                else {
+                    curr[j] = false;
+                }
+            }
+            next = curr;
+        }
+
+        return next[0];
+    }
+
+public:
+    bool isMatch(string s, string p) { return solve(s, p); }
+};
